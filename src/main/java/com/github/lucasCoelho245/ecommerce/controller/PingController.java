93394efaad6d86610bbda5c66c10739722e0229c
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2023-09-29 02:46:43 -0300

    Create PingController.java

---
+++ src/main/java/com/github/lucasCoelho245/ecommerce/controller/PingController.java
@@ -0,0 +1,15 @@
+package com.github.lucasCoelho245.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong ";
+    }
+}
